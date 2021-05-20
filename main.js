d57522fe472abccc51aa900fc28c0400de98b338
Hemanshu <hemanshu95@gmail.com>
2021-05-21 02:03:49 +0530

    make it better

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 const jsonParser = bodyParser.json()
-const host = "0.0.0.0"
 
 //app.use(express.bodyParser());
 
@@ -13,19 +12,42 @@ const host = "0.0.0.0"
 //   app.use(app.router);
 // });
 var toSend = {};
+var groups = {};
 
 app.get('/test', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/group/:groupId', (req, res) => {
+  res.sendFile(path.join(__dirname+'/groupcall.html'));
+})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/test.html'));
 })
 
-app.get('/group', (req, res) => {
-  res.sendFile(path.join(__dirname+'/groupcall.html'));
+
+app.post('/group/:groupId/register', jsonParser ,(req, res) => {
+  var groupId = req.params.groupId;
+  var userName = req.body.userName;
+  var userId = Date.now() + userName;
+  if(groups[groupId]){
+      for(index in groups[groupId]){
+          addToSend(groups[groupId][index], userId, userId, "new_caller");
+      }
+      groups[groupId].push(userId);
+  }
+  else
+      groups[groupId] = [userId];   
+  res.send({"userId":userId});
 })
+function addToSend(receiver, sender, msg, type){
+    var payload = {'sender' : sender, 'msg' : msg, 'type' : type };
+    if(toSend[receiver])
+      toSend[receiver].push(payload);
+    else
+      toSend[receiver] = [payload];
+}
 
 app.post('/sendMsg', jsonParser,(req, res) =>{
 
@@ -34,11 +56,7 @@ app.post('/sendMsg', jsonParser,(req, res) =>{
     var receiver = req.body.receiver;
     var msg = req.body.msg;
     var type = req.body.type;
-    var payload = {'sender' : sender, 'msg' : msg, 'type' : type };
-    if(toSend[receiver])
-    	toSend[receiver].push(payload);
-    else
-    	toSend[receiver] = [payload];
+    addToSend(receiver, sender, msg, type);
    	console.log(toSend); 
     res.sendStatus(200);
 
@@ -47,15 +65,15 @@ app.post('/sendMsg', jsonParser,(req, res) =>{
 app.post('/getMsg', jsonParser,(req, res) =>{
 
 	console.log('Got body:', req.body);
+  console.log('groups', groups);
     var sender = req.body.sender;
     res.send(toSend[sender]);
     delete toSend[sender];
     console.log(toSend);
- 	//res.send(req.json());
 
 })
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://${host}:${PORT}`)
+  console.log(`Example app listening at port ${PORT}`)
 })
